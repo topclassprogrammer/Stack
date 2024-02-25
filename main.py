ce5185c8d3221b2d9926a62557df565899a6aa83
Ivan Golovinov <topclassprogrammer@gmail.com>
2024-02-25 17:22:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+        return None
+
+    def peek(self):
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
+
+
+if __name__ == '__main__':
+    s = Stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    s.size()
+    s.pop()
+    s.pop()
+    s.peek()
+    s.pop()
+    s.is_empty()
